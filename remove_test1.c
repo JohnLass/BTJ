3943212245444c66a6806dff1fb7f72da76d4c7c
BobbyCarangelo <robert.f.carangelo.iii.21@dartmouth.edu>
2020-10-05 17:22:26 -0400

    10.5.2020 5:22 PM EST

--- remove_test1.c
+++ remove_test1.c
@@ -17,55 +17,47 @@
 #include "listfun.h"
 
 int main(void){
-
-	
-	// Initialize three pointers for list and one tester
 	car_t * firstp = makecar("8008135", 60.4, 2020);
 	car_t * secondp = makecar("p", 6020.2, 2003);
 	car_t * lastp = makecar("rfc47", 3932.9, 20120);
-	car_t * testp = (car_t *) malloc(sizeof(car_t));
+	car_t * testp;
 
-	//Make a 3 car list in the correct order
 	lput(lastp);
 	lput(secondp);
 	lput(firstp);
-
-	//assign the first car to be removed and put in test pointer
-	testp = lremove("8008135");
-
-
 	
+	testp = lremove("8008135");	
 	//make sure that test pointer matches datat that was in the first list item
-	if(strcmp(testp->plate,"8008135") != 0){
+	if(!checkcar(testp,"8008135", 60.4, 2020)){
+		printf("Failure");
 		exit(EXIT_FAILURE);
 	}
-	if(testp->price != 60.4){
-		exit(EXIT_FAILURE);
-	}
-
+	free(testp);
 	//Call lget, to make sure that the first thing actually got removed
 	testp = lget();
-
-
 	//Check to make sure that testp now  matches second entry data
-	if(strcmp(testp->plate,"p") != 0){
+	if(!checkcar(testp,"p",6020.2,2003)){
+		printf("Failure");
 		exit(EXIT_FAILURE);
 	}
-	if(testp->price != 6020.2){
-		exit(EXIT_FAILURE);
-	}
-
+	free(testp);
+	
 	//try removing something that isnt there
 	testp = lremove("hahalmao");
-
-	//should be null if the plate didnt exist
 	if(testp != NULL){
+		printf("Failure");
 		exit(EXIT_FAILURE);
 	}
-
-
-
-	//if it got to here it was a successful test
+	
+	//try removing the last one
+	testp = lremove("rfc47");
+	
+	if(!(checkcar(testp,"rfc47", 3932.9, 20120))){
+		printf("Failure");
+		exit(EXIT_FAILURE);
+	}
+	free(testp);
+	
 	exit(EXIT_SUCCESS);
 	
 }
