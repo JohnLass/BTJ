cd56a428b360762a16ea5ffa8d64803511f8e64e
John Lass <John.J.Lass.TH@dartmouth.edu>
2020-10-05 16:13:40 -0400

    10.05.2020:4:13

--- gettest1.c
+++ gettest1.c
@@ -11,86 +11,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "list.h"
 #include "listfun.h"
 
 int main(void){
-
-	//initialize variables
+	bool carstat1, carstat2;
+	car_t *p1, *p2, *lp, *tp;
+		
+	p1 = makecar("828393", 800.5, 2005);
+	p2 = makecar("GOOSE", 700.5, 2015);
+	lp = makecar("last1", 600.5, 2005);
 	
-	car_t *firstp = (car_t *) malloc(sizeof(car_t));
-	car_t *secondp = (car_t *) malloc(sizeof(car_t));
-	car_t *lastp = (car_t *) malloc(sizeof(car_t));
-	car_t *testp = (car_t *) malloc(sizeof(car_t));
-
-	//initialize the cars with data
-	firstp->next = NULL;
-	strcpy(firstp->plate,"828393");
-	firstp->price = 800.5;
-	firstp->year = 2005;
-	
-
-	secondp->next = NULL;
-	strcpy(secondp->plate,"GOOSE");
-	secondp->price = 700.5;
-	secondp->year = 2105;
-	
-	lastp->next = NULL;
-	strcpy(lastp->plate,"last1");
-	lastp->price = 600.5;
-	lastp->year = 2015;
-
-	//Link items in list together
-	//firstp->next = secondp;
-	//	secondp->next = lastp;
 	lput(lastp);
 	lput(secondp);
 	lput(firstp);
 
-	//run test to see if we can get the first car (and delete it)
-	
 	testp = lget();
-
-
-	//check to make sure data in test pointer is correct (matches the first pointer in the list)
 
 	printf("this is testp->price: %lf\n", testp->price);
 	printf("this is firstp->price: %lf\n", firstp->price);
-	
-	if(testp->price != firstp->price){
-		exit(EXIT_FAILURE);
-	}
 
-	if(strcmp(testp->plate,firstp->plate) != 0){
-		exit(EXIT_FAILURE);
-	}
-
-	if(testp->next != secondp){
+	if((checkcar(testp,"828393", 800.5, 2005))==FALSE) {
+		printf("Failure");
 		exit(EXIT_FAILURE);
 	}
 	
-
-	//run it again and make sure it now has data for second pointer
-
+	
 	testp = lget();
-
-
-	// check test pointer data to make sure it has second pointer's data now
-
-	if(testp->price != secondp->price){
-		exit(EXIT_FAILURE);
-	}
-
-	if(strcmp(testp->plate,secondp->plate) != 0){
-		exit(EXIT_FAILURE);
-	}
-
-	if(testp->next != lastp){
-		exit(EXIT_FAILURE);
-	}
-
-	//	free(firstp);
-	//	free(secondp);
+carstat2 = 
 	free(lastp);
 	free(testp);
 	
